fae09ff5ed04924d84b742dca8451eec29a9de53
Mauricio Zamora <mauricio.zamorahernandez@ucr.ac.cr>
2024-05-20 18:38:14 -0600

    Versión Inicial

--- mrp/proveedor.py
+++ mrp/proveedor.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, g, abort
 from mrp.auth import login_required
-from .models import Proveedor,Unidad
+from .models import Proveedor, Lugar, Unidad
 from mrp import db
 from sqlite3 import IntegrityError
 
@@ -24,58 +24,35 @@ def index():
 @bp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
-    # categorias_proveedor = (CategoriaProveedor
-    #                        .query.filter_by(esta_activo=True)
-    #                        .order_by(CategoriaProveedor.nombre).all())
-    # unidades = (Unidad.query
-    #             .filter_by(esta_activo=True)
-    #             .order_by(Unidad.nombre).all())
+    lugares = (Lugar
+               .query.filter_by(esta_activo=True)
+               .order_by(Lugar.nombre).all())
 
     if request.method == 'POST':
-        sku = request.form['sku']
-        categoria_proveedor_id = request.form['categoria-proveedor']
+        identificacion = request.form['identificacion']
+        lugar_id = request.form['lugar']
         nombre = request.form['nombre']
-        unidad_id = request.form['unidad']
-        cantidad_total = request.form['cantidad-total']
-        costo = request.form['costo']
-        porcentaje_impuesto = request.form['porcentaje-impuesto']
-        redondeo = request.form['redondeo']
-        modo_utilidad = request.form['modo-utilidad']
-        utilidad_monto = 0
-        utilidad_porcentaje = 0
-        utilildad_es_porcentaje = True
-        if modo_utilidad == 'porcentaje':
-            utilidad_porcentaje = request.form['utilidad-porcentaje']
-            utilildad_es_porcentaje = True
-        else:
-            utilidad_porcentaje = 0
-        if utilidad_monto == 'monto':
-            utilidad_monto = request.form['utilidad-monto']
-            utilildad_es_porcentaje = False
-        else:
-            utilidad_porcentaje = 0
+        correo_electronico = request.form['correo-electronico']
+        telefono = request.form['telefono']
+        es_persona_fisica = request.form.get('es-persona-fisica') == 'on'
         esta_activo = request.form.get('esta-activo') == 'on'
 
-        proveedor = Proveedor(sku=sku,
-                            nombre=nombre,
-                            categoria_proveedor_id=categoria_proveedor_id,
-                            unidad_id=unidad_id,
-                            cantidad_total=cantidad_total,
-                            costo=costo,
-                            porcentaje_impuesto=porcentaje_impuesto,
-                            redondeo=redondeo,
-                            utilildad_es_porcentaje=utilildad_es_porcentaje,
-                            porcentaje_utilidad=utilidad_porcentaje,
-                            monto_utilidad=utilidad_monto,
-                            esta_activo=esta_activo
-                            )
+        proveedor = Proveedor(identificacion=identificacion,
+                          lugar_id=lugar_id,
+                          nombre=nombre,
+                          correo_electronico=correo_electronico,
+                          telefono = telefono,
+                          es_persona_fisica = es_persona_fisica,
+                          esta_activo=esta_activo
+                          )
 
         db.session.add(proveedor)
-
+        id: int = -1
         # db.session.commit()
 
         try:
             db.session.commit()
+            id = proveedor.id
         except AssertionError as err:
             db.session.rollback()
             abort(409, err)
@@ -88,66 +65,40 @@ def create():
         finally:
             db.session.close()
 
-        return redirect(url_for('proveedor.index'))
+        return redirect(url_for('proveedor.view', id=id))
 
-    return render_template('proveedor/create.html')
+    return render_template('proveedor/create.html', lugares=lugares)
 
 
 @bp.route('/update/<int:id>', methods=['GET', 'POST'])
 @login_required
 def update(id):
     proveedor = get(id)
-    # categorias_proveedor = (CategoriaProveedor
-    #                        .query.filter_by(esta_activo=True)
-    #                        .order_by(CategoriaProveedor.nombre).all())
-    unidades = (Unidad.query
-                .filter_by(esta_activo=True)
-                .order_by(Unidad.nombre).all())
+    lugares = (Lugar
+               .query.filter_by(esta_activo=True)
+               .order_by(Lugar.nombre).all())
 
     if request.method == 'POST':
-        proveedor.sku = request.form['sku']
-        proveedor.categoria_proveedor_id = request.form['categoria-proveedor']
+        proveedor.identificacion = request.form['identificacion']
+        proveedor.lugar_id = request.form['lugar']
         proveedor.nombre = request.form['nombre']
-        proveedor.unidad_id = request.form['unidad']
-        proveedor.cantidad_total = request.form['cantidad-total']
-        proveedor.costo = request.form['costo']
-        proveedor.porcentaje_impuesto = request.form['porcentaje-impuesto']
-        proveedor.redondeo = request.form['redondeo']
-        modo_utilidad = request.form['modo-utilidad']
-        utilidad_monto = 0
-        utilidad_porcentaje = 0
-        utilildad_es_porcentaje = True
-        if modo_utilidad == 'porcentaje':
-            utilidad_porcentaje = request.form['utilidad-porcentaje']
-            utilildad_es_porcentaje = True
-        else:
-            utilidad_porcentaje = 0
-
-        if utilidad_monto == 'monto':
-            utilidad_monto = request.form['utilidad-monto']
-            utilildad_es_porcentaje = False
-        else:
-            utilidad_porcentaje = 0
-
-        proveedor.utilidad_porcentaje = utilidad_porcentaje
-        proveedor.utilidad_monto = utilidad_monto
-        proveedor.utilildad_es_porcentaje = utilildad_es_porcentaje
-
+        proveedor.correo_electronico = request.form['correo-electronico']
+        proveedor.telefono = request.form['telefono']
+        proveedor.es_persona_fisica = request.form.get('es-persona-fisica') == 'on'
         proveedor.esta_activo = request.form.get('esta-activo') == 'on'
 
         db.session.commit()
 
-        return redirect(url_for('proveedor.index'))
+        return redirect(url_for('proveedor.view', id=id))
 
-    # return render_template('proveedor/update.html', proveedor=proveedor, categorias_proveedor=categorias_proveedor,
-    #                        unidades=unidades)
+    return render_template('proveedor/update.html', dato=proveedor, lugares=lugares)
 
 
 @bp.route('/view/<int:id>')
 @login_required
 def view(id):
     proveedor = get(id)
-    return render_template('proveedor/view.html', proveedor=proveedor)
+    return render_template('proveedor/view.html', dato=proveedor)
 
 
 @bp.route('/delete/<int:id>', methods=['GET', 'POST'])
@@ -160,4 +111,4 @@ def delete(id):
         db.session.commit()
         return redirect(url_for('proveedor.index'))
 
-    return render_template('proveedor/delete.html', proveedor=proveedor)
+    return render_template('proveedor/delete.html', dato=proveedor)
